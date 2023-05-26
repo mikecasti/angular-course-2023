af9d312c800e853d9ad23dbe8422be7710f3c1d6
mikecasti <mikecasti@yahoo.com>
2023-05-26 15:12:02 -0400

    [20230526 task 1] examples of arrow functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'course-rsoft';
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  getName(name: string): string {
+    return 'constructor ' + name;
+  }
+
+  //función Flecha
+  // const getName1 = (name:string) => 'Hi ' + name;
+
+  // (a: number, b: number) => a + b + 100;
+
+  // let imprime = (mensaje) => {log}
+
+  // const ddd = (msg: string) => {msg};
 }
